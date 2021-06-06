317ee4fefe3dafbb18a0fe9a69a2f561a941a51d
kasai9244 <>
2021-06-06 02:28:54 +0000

    I added the code to create a function called create_point_geom() that has two parameters.

--- Exercise_1_problem_1_2.py
+++ Exercise_1_problem_1_2.py
@@ -11,7 +11,8 @@
 
 from shapely.geometry import Point, LineString, Polygon
 #YOUR CODE HERE 1 to define create_point_geom()
-
+def create_point_geom(x,y):
+  return Point(x,y)
 # Test your function by running these code cells:
 
 # CODE FOR TESTING YOUR SOLUTION
